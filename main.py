1d5df65335c54b5fe64037de07295e0b685744f3
Kartik Lamkhade <kartiklamkhade7@gmail.com>
2026-08-23 21:02:30 +0530

    add data

--- main.py
+++ main.py
@@ -1,11 +1,66 @@
+# from fastmcp import FastMCP
+# import random
+# mcp = FastMCP(name='Demo_server')
+
+# @mcp.tool
+# def rool_dice(n_dice: int) -> list[int]:
+#     "this function use to rolle n_dice"
+#     return [random.randint(1,6) for i in range(n_dice)]
+
+# @mcp.tool
+# def add(a: float,b: float) -> float:
+#     'this function add tow numbers'
+#     return a+b
+
+# if __name__ == "__main__":
+#     mcp.run()
+
+
+
 from fastmcp import FastMCP
+import sqlite3
+import os
+mcp = FastMCP(name='expainse_tracker')
+path = os.path.join(os.path.dirname(__file__),'data.db')
+with sqlite3.connect(path) as f:
+    f.execute("""
+                CREATE TABLE IF NOT EXISTS expense (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    date TEXT NOT NULL,
+                    amount REAL NOT NULL,
+                    category TEXT NOT NULL,
+                    subcategory TEXT DEFAULT ''
+                );
+            """)
 
-mcp = FastMCP(name="My FastM")
 
-@mcp.tool()
-def add(a: int, b: int):
-    '''Add two numbers together.'''
-    return a + b
+@mcp.tool
+def add_expense(date ,amount: int,category: str,subcategory: str):
+    'this function use to add expense in expense tracker'
+    with sqlite3.connect(path) as f:
+        cur = f.execute("INSERT INTO expense(date,amount,category,subcategory) VALUES (?,?,?,?)",
+                        (date,amount,category,subcategory))
+        return {'status':'ok','id':cur.lastrowid}
 
-if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+
+@mcp.tool
+def list_all_expense():
+    'this function return all expense data'
+    with sqlite3.connect(path) as f:
+        data = f.execute("SELECT id,date,amount,category,subcategory FROM expense ORDER BY id ASC")
+        cur = [d[0] for d in data.description]
+        return [dict(zip(cur,r)) for r in data.fetchall()]
+
+
+@mcp.tool
+def list_expense_in_range(st,ed):
+    'this function return expense in range of dates form data'
+    with sqlite3.connect(path) as f:
+        data = f.execute("SELECT id,date,amount,category,subcategory FROM expense WHERE date BETWEEN ? AND ? ORDER BY id ASC",
+                         (st,ed))
+        cur = [d[0] for d in data.description]
+        return [dict(zip(cur,r)) for r in data.fetchall()]
+
+if __name__ == '__main__':
+
+    mcp.run()

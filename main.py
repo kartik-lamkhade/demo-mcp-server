f351aed24ae484c6a081106b11febd9edd906e9d
Kartik Lamkhade <kartiklamkhade7@gmail.com>
2026-08-23 22:09:50 +0530

    add aiosqlite

--- main.py
+++ main.py
@@ -20,13 +20,16 @@
 from fastmcp import FastMCP
 import sqlite3
 import os
+import aiosqlite
+import asyncio
 import tempfile
-
+mcp = FastMCP(name='expainse_tracker')
+path = os.path.join(tempfile.gettempdir(),'data.db')
 try:
-    mcp = FastMCP(name='expainse_tracker')
-    path = os.path.join(tempfile.gettempdir(),'data.db')
-    with sqlite3.connect(path) as f:
-        f.execute("""
+    async def create():
+
+        async with aiosqlite.connect(path) as f:
+            await f.execute("""
                     CREATE TABLE IF NOT EXISTS expense (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         date TEXT NOT NULL,
@@ -34,36 +37,38 @@ try:
                         category TEXT NOT NULL,
                         subcategory TEXT DEFAULT ''
                     );
-                """)
+                    """)
+                    
+
 except Exception as e:
     raise Exception(f"Error initializing the database: {e}")
-
+create()
 @mcp.tool
 async def add_expense(date ,amount: int,category: str,subcategory: str):
         'this function use to add expense in expense tracker'
-        with sqlite3.connect(path) as f:
-            cur = f.execute("INSERT INTO expense(date,amount,category,subcategory) VALUES (?,?,?,?)",
-                            (date,amount,category,subcategory))
-            return {'status':'ok','id':cur.lastrowid}
+        async with aiosqlite.connect(path) as f:
+                cur = await f.execute("INSERT INTO expense(date,amount,category,subcategory) VALUES (?,?,?,?)",
+                                (date,amount,category,subcategory))
+                return {'status':'ok','id':cur.lastrowid}
 
 
 @mcp.tool
 async def list_all_expense():
     'this function return all expense data'
-    with sqlite3.connect(path) as f:
-        data = f.execute("SELECT id,date,amount,category,subcategory FROM expense ORDER BY id ASC")
+    async with aiosqlite.connect(path) as f:
+        data = await f.execute("SELECT id,date,amount,category,subcategory FROM expense ORDER BY id ASC")
         cur = [d[0] for d in data.description]
-        return [dict(zip(cur,r)) for r in data.fetchall()]
+        return [dict(zip(cur,r)) for r in await data.fetchall()]
 
 
 @mcp.tool
 async def list_expense_in_range(st,ed):
     'this function return expense in range of dates form data'
-    with sqlite3.connect(path) as f:
-        data = f.execute("SELECT id,date,amount,category,subcategory FROM expense WHERE date BETWEEN ? AND ? ORDER BY id ASC",
+    async with aiosqlite.connect(path) as f:
+        data = await f.execute("SELECT id,date,amount,category,subcategory FROM expense WHERE date BETWEEN ? AND ? ORDER BY id ASC",
                          (st,ed))
         cur = [d[0] for d in data.description]
-        return [dict(zip(cur,r)) for r in data.fetchall()]
+        return [dict(zip(cur,r)) for r in await data.fetchall()]
 
 if __name__ == '__main__':
 
